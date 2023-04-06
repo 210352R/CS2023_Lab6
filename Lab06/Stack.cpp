0ec0754aec2e791dd18f3e476b690b76d95d4127
Eshan_Maduranga <127854691+210352R@users.noreply.github.com>
2023-04-06 14:59:23 +0530

    add Lab06

---
+++ Lab06/Stack.cpp
@@ -0,0 +1,123 @@
+#include <iostream>
+#include <ctime>
+#include <chrono>
+using namespace std;
+using namespace std::chrono;
+
+
+struct Node{
+    int data;
+    struct Node* next;
+};
+
+// stack Size ;
+const int stack_size = 100;
+int sizes = 0;
+
+//Head pointer ---
+struct Node* head = NULL;
+
+bool isEmpty(){
+    return head == NULL;
+}
+
+bool isFull(){
+    return sizes == stack_size;
+}
+
+
+//insert value to start --
+void Push(int new_data) {
+   if(!isFull()){
+       struct Node* new_node = (struct Node*) malloc(sizeof(struct Node));
+       new_node->data = new_data;
+       if(head==NULL){
+        head = new_node;
+        head->next = NULL;
+        sizes++;
+       }else{
+           new_node->next = head;
+           head = new_node;
+           sizes++;
+       }
+   }else{
+       cout<<"Stack is OverFlow"<<endl;
+   }
+
+}
+
+int Pop(){
+    if(!isEmpty()){
+        struct Node* ptr;
+        ptr = head;
+        head = head->next;
+        sizes--;
+        return ptr->data;
+    }else{
+        cout<<"Stack is Empty --"<<endl;
+    }
+}
+
+
+int StackTop(){
+    if(isEmpty()){
+        cout<<"Stack is Empty"<<endl;
+        return -1;
+
+    }else{
+        return head->data;
+    }
+}
+
+void Display() {
+    if(!isEmpty()){
+       struct Node* ptr;
+       ptr = head;
+       while (ptr != NULL) {
+          cout<< ptr->data <<" ";
+          ptr = ptr->next;
+       }
+       cout<<endl;
+    }else{
+        cout<<"Stack is Empty"<<endl;
+    }
+}
+
+int main(){
+
+    double startTime = clock();
+
+    Push(8);
+    Push(10);
+    Push(5);
+    Push(11);
+    Push(15);
+    Push(23);
+    Push(6);
+    Push(18);
+    Push(20);
+    Push(17);
+
+    Display();
+
+    for (int i=0;i<5;i++){
+        Pop();
+    }
+    Display();
+    Push(4);
+    Push(30);
+    Push(3);
+    Push(1);
+    Display();
+
+    double endTime = clock();
+
+    double execution_time = double(endTime- startTime) / CLOCKS_PER_SEC;
+
+    cout << "Execution time: " << execution_time << " seconds" << std::endl;
+
+
+    return 0;
+}
+
+
